31e2eb58004a4fed40b6f6ce080524a026d0e48d
cauvery-digital <geetaelectronicsandinfosys@gmail.com>
2022-11-03 08:47:41 +0530

    first commit

---
+++ src/pages/Sitemap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sitemap () {
+  return (
+    <div>
+      <h1 className='pt-5 mt-5 pl-5'>This is a Sitemap Page</h1>
+    </div>
+  )
+}
+
+export default Sitemap
